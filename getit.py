18f52efe140a53babc37c5d6588b4dd94eda2a11
ankitjain28may <ankitjain28may77@gmail.com>
2016-12-10 19:59:22 +0530

    Added more features to script

--- getit.py
+++ getit.py
@@ -5,9 +5,11 @@ import time
 from colorama import Fore, Style
 from colorama import init
 init()
+
+
 # ================================Error Function=========================================
 def usage():
-    print("\n\t==========Invalid Input=============")
+    print(Fore.YELLOW + "\n\t==========Invalid Input=============")
     usage = """
     -h --help                 Print This
     -d --url                  Download Url
@@ -16,105 +18,117 @@ def usage():
     print(usage)
     print("\n\t===============End==================")
 
+
+# ================================Progress Bar Function=========================================
+
 def progressBar(length):
     show = ''
-    # return length
     for i in range(length):
         show+="#"
     for j in range(50-length):
         show+="-"
-    return "["+show+"]"
+    show = "["+show+"]"
+    if length!=50:
+        if length % 3 == 0:
+            show = "/ " + show
+        elif length % 3 == 1:
+            show = "- " + show
+        else:
+            show = "\ " + show
+    return show
+
+# ================================Transfer Speed Function=========================================
 
 def transferRate(blocksize):
     try:
-        global speed, t, transferData
-        interval = time.time() - t
-        if t == 0:
+        global speed, rate, transferData
+        interval = time.time() - rate
+        if rate == 0:
             transferData += blocksize
-            t = time.time()
+            rate = time.time()
         elif interval == 0 or interval < 1:
             transferData += blocksize
         else:
             speed = float(transferData/(interval))
             transferData = 0
-            t = time.time()
+            rate = time.time()
         return speed
     except Exception:
         pass
 
+# ================================Type of Data=========================================
+
 def dataSize(block):
     if block/1024 < 1000:
         block = block/1024
-        factor = 1
         sizeType = 'KB'
     elif block/1048576 < 1000:
         block = block/1048576
-        factor = 2
         sizeType = 'MB'
     elif block/1073741824 < 1000:
         block = block/1073741824
-        factor = 3
         sizeType = 'GB'
-    return block, factor, sizeType
+    return block, sizeType
 
+# ================================Main Integrating function=========================================
 
-def reporthook(blocknum, blocksize, totalsize):
-    desc = int(blocknum*blocksize)
-    sizeType = ''
+def reporthook(blocknum, blocksize, total):
     size = 0
-    factor = 0
-    length = int(desc/totalsize*50)
-    percentage = float(desc/totalsize*100)
-    global speed, speedData
+    currentType = ''
+    desc = int(blocknum*blocksize)
+    length = int((desc/total)*50)
+    percentage = float(desc/total*100)
+    global speed, speedType, totalsize, sizeType
     speed = transferRate(blocksize)
-    speedShow, factor, speedData = dataSize(speed)
-    totalsize, factor, sizeType = dataSize(totalsize)
-    if factor == 1:
-        size = float(desc/1024)
-    elif factor == 2:
-        size = float(desc/1048576)
-    elif factor == 3:
-        size = float(desc/1073741824)
+    speedShow, speedType = dataSize(speed)
+    if totalsize == 0:
+        totalsize, sizeType = dataSize(total)
+    size, currentType = dataSize(desc)
+    progress = progressBar(length)
 
     if percentage > 100:
         percentage = 100
         size = totalsize
-    progress = progressBar(length)
-    # s = "\r%.2f %% %s %.2f %s / %.2f %s %.2f %s/sec " % (percentage, progress, size, sizeType, totalsize, sizeType, speedShow, speedData)
     p1 = "\r %.2f %% " %(percentage)
     p2 = "%s " %(progress)
-    p3 = "%.2f %s / %.2f %s %.2f %s/sec " %(size, sizeType, totalsize, sizeType, speedShow, speedData)
-    sys.stdout.flush()
+    p3 = "%.2f %s / %.2f %s %.2f %s/sec " %(size, currentType, totalsize, sizeType, speedShow, speedType)
     sys.stderr.write(p1 + Fore.GREEN + p2)
     sys.stderr.write(Style.RESET_ALL)
     sys.stderr.write(p3)
     sys.stderr.write(Style.RESET_ALL)
     sys.stdout.flush()
 
+# ================================Space separated name amendment=========================================
+
 def nameAmend(name):
     name = name.split(' ')
     name = '_'.join(name)
     return name
 
-# =====================================Main Body=========================================
+# =====================================Globar Variables=========================================
 
 argv = sys.argv[1:]
 url = ''
 name = 'default'
 opts = ''
 args = ''
-t = 0
+rate = 0
 speed = 0
-speedData = ''
 transferData = 0
+totalsize = 0
+speedType = ''
+sizeType = ''
+
+
 # ================================Command Line Input=====================================
 
 try:
     opts, args = getopt.getopt(argv, "hd:f:", ["help", "url=", "filename="])
 except getopt.GetoptError as err:
     usage()
-    print(str(err))
+    print(Fore.RED + str(err))
     sys.exit(2)
+
 # ================================Read the CLI Input=====================================
 for opt, arg in opts:
     if opt in ("-h", "--help"):
@@ -131,6 +145,7 @@ if url == '':
     url = input("Enter the Download-Url\n")
 if name == 'default':
     name = input("Enter the name of the File with the extension\n")
+
 # ================================Download the file======================================
 try:
     name = nameAmend(name)
@@ -139,5 +154,5 @@ try:
     print("\n")
     print("Download completed..!!\n")
 except Exception as e:
-    print(e)
+    print(Fore.RED + e)
 
